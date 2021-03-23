c3298ee25f052388546003ed93d14a9270623cbb
Bharat Bhusan <bh27122000@gmail.com>
2021-03-23 23:48:16 +0530

    modified

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express=require('express');
 const app=express();
+const path=require('path')
 
-
-app.use(express.static(__dirname+'public'));
+app.use('/public',express.static(path.join(__dirname, 'public')));
 
 app.set('view engine','ejs');
 
@@ -11,7 +11,7 @@ app.use('/',(req,res)=>{
 })
 
 const port=5000
+
 app.listen(port,()=>{
     console.log(`Listning at the port: ${port}`)
 })
-
